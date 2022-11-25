f547d5042edac92b16e883f74cfa799a9f952430
Амелькин Владимир <amelkin@vbankcenter.ru>
2022-11-25 17:33:04 +0300

    Added historical event cache

---
+++ Kraeved/Helpers/UserDefaultsKeys.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultKeys.swift
+//  Kraeved
+//
+//  Created by Владимир Амелькин on 25.11.2022.
+//
+
+import Foundation
+
+enum UserDefaultsKeys: String {
+    case historicalEvents
+}
